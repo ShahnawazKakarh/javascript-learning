6b5953f400ae9f6ab5440f3166f483bc39716418
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-05-28 21:38:30 +0000

    day 2 and data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let name = "Shahnawaz"
+let age = "22"
+let isLoggedIn = true
+
+// Data Types
+
+// Number 
+// BigInt => mainly used in trading apps
+// String => ""
+// Boolean => true/false
+// null => standalone value, "" is not null its string, you have to define null to identify null
+// undefined => 
+// Symbol => to find uniqueness
+
+// Object => 
+
+// console.log(typeof "Kakarh") => String
+// console.log(typeof undefined) = > undefined={}
+// console.log(typeof null) => Object
