a1eb08fef1666d627764c25c75ebaa24930f8df9
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-06-04 21:37:11 +0000

    comparisions added

--- 01_basics/03_operations.js
+++ 01_basics/03_operations.js
@@ -28,3 +28,29 @@ console.log(1 + "2") // 12
 console.log("1" + 2 + 2) // 122
 console.log(1 + 2 + "2") // 32
 console.log(1 + 2 + 2) // 5
+
+console.log(true) // true
+console.log(+true) // 1
+console.log(+"") // 0
+
+
+// Another Example
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+
+// prefix and postfix value in js
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
