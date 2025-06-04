068fbd0bb2c626f445ba6b06eb52449f5bb8ccc0
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-06-04 20:30:51 +0000

    Operations and Datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,6 @@
-let name = "Shahnawaz"
-let age = "22"
-let isLoggedIn = true
+// "use Strict"; // treat all JS code as new version
+
+// alert (3 + 3) // it is being used in browser but here we are using nodejs
 
 // Data Types
 
@@ -9,11 +9,33 @@ let isLoggedIn = true
 // String => ""
 // Boolean => true/false
 // null => standalone value, "" is not null its string, you have to define null to identify null
-// undefined => 
+// undefined => variable assign kia hai but uasko value nhn assign ki
 // Symbol => to find uniqueness
-
 // Object => 
 
 // console.log(typeof "Kakarh") => String
 // console.log(typeof undefined) = > undefined={}
 // console.log(typeof null) => Object
+
+
+// TYPE OF DATA TYPE VALUES WHILE CHANGING THEM
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1; false => 0
+
+score = 33
+second_score = "44"
+third_score = "444abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(second_score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+console.log(typeof second_score); // type of is string 
+console.log(typeof Number(second_score)); // this data type with Capital Number will convert it into number
+
+console.log(typeof third_score);
+
---
+++ 01_basics/03_operations.js
@@ -0,0 +1,30 @@
+// const { StrictMode } = require("react")
+
+// let name = "Shahnawaz"
+// let age = "22"
+// let isLoggedIn = true
+
+/* Some OPERATIONS in JS */
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2); // reminder/modulus
+
+// Adding two strings
+str = "Hello Shahnawaz"
+str2 = "ShahKhan"
+str3 = str + str2
+console.log(str3)
+
+
+// Adding numbers in strings
+// toprimitive input conversion into preferred types - ECMA guidelines
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+
+// if a string is used it will just concatenate
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+console.log(1 + 2 + 2) // 5
