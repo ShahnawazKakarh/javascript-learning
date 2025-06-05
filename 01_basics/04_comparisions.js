1d223b9bc401034b76bd678c16020a4ac4deadf4
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-06-05 19:57:31 +0000

    comparisions added

--- 01_basics/04_comparisions.js
+++ 01_basics/04_comparisions.js
@@ -1,4 +1,8 @@
-// 
+/* KEY POINT */
+// Comparison '>=', '<=', '>' , '<' and Equality operater '==' check both are different
+// suppose, console.log(null >= 0) , here comparision has converted null into zero so zero >= zero is true
+// while,  console.log(null > 0) , here it is false as it is not comparision so null cannot be greater than zero
+
 console.log(2 > 1);
 console.log(2 < 1);
 console.log(2 >= 1);
@@ -16,4 +20,20 @@ console.log(undefined > 1)
 console.log(undefined < 1)
 
 
-console.log("2" === 2)
+// Not comparing same data type
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// Strict Check , '==='
+// Strictly check not only the value even strictly check the values like their data types
+console.log("2" === 2);
