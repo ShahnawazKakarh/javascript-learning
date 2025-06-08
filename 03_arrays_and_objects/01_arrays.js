d282de1e73eacadf2bd0b67e5317142bf22725c8
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-06-08 21:24:22 +0000

    basics to advanced array part 1

--- 03_arrays_and_objects/01_arrays.js
+++ 03_arrays_and_objects/01_arrays.js
@@ -11,32 +11,79 @@ console.log(myArr[1]);
 
 // Array methods
 
+// push array
 myArr.push(6)
-myArr.push(7)
-myArr.pop()
+console.log(myArr)
 
-myArr.unshift(9)
-myArr.shift()
+// pop array
+myArr2.pop(8)
+console.log(myArr2)
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3));
+// pop array
+myHeors.pop(7)
+console.log(myHeors)
 
-const newArr = myArr.join()
+// unshift
+myArr.unshift(0)
+console.log(myArr)
 
-console.log(myArr);
-console.log( newArr);
+// shift
+myArr.shift(0)
+console.log(myArr)
+
+// join
+myArr.join()
+console.log(myArr)
 
 
-// slice, splice
+// // slice, splice
 
+// printing my original array
 console.log("A ", myArr);
 
+// creating a constant and putting sliced value in it
+// slice (1,3) mean it will include 1st and 2nd index but ignore third index
 const myn1 = myArr.slice(1, 3)
 
+// it will print value which will be sliced i.e., [ 1, 2 ]
 console.log(myn1);
+
+// now I am printing the same my old array B  [0, 1, 2, 3,4, 5, 6]
 console.log("B ", myArr);
 
-
+// now we are doing splice it will take 1st, 2nd and 3rd index
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
+
+// array value was [0,1,2,3,4,5,6] it will print [ 1, 2, 3 ] as they are 1st, 2nd and 3rd index
 console.log(myn2);
+
+// now if I print my old original array
+// it has value C  [ 0, 4, 5, 6 ]
+// because splice also remove those indexed value
+console.log("C ", myArr);
+
+
+/* ******************************************** ADVANCE EXAMPLE *********************************************** */
+
+
+// More Advanced Example of Slice
+const arry = [1, 2, 3, 4, 5];
+const newArr = arry.slice(1, 4);
+
+console.log(newArr); // [2, 3, 4]
+console.log(arry);    // [1, 2, 3, 4, 5] → original unchanged
+
+
+// More Advanced Example of Splice
+const ary = [1, 2, 3, 4, 5];
+
+// Remove 2 elements starting from index 1
+const removed = ary.splice(1, 2);
+
+console.log(removed); // [2, 3]
+console.log(ary);     // [1, 4, 5] → original changed
+
+// Insert elements at index 1
+ary.splice(1, 0, 'a', 'b');
+console.log(ary);     // [1, 'a', 'b', 4, 5]
+
