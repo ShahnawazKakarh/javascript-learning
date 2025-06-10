cd133b8ad2ce5653b1475dda98f5e5d14fd3120d
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-06-10 11:39:02 +0000

    adv obj

--- 03_arrays_and_objects/04_advance_objects.js
+++ 03_arrays_and_objects/04_advance_objects.js
@@ -7,35 +7,54 @@
 // Useful for entities (users, items)
 
 
-const tinderUserS = new Object()  // singelton object
-const tinderUserNS = {}  // non-singelton object
-// console.log(tinderUser);
+// const tinderUserS = new Object()  // singelton object
+// const tinderUserNS = {}  // non-singelton object
 
-
+const tinderUser = {}
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
+// console.log(tinderUser);
 
 
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
         userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
+            firstname: "Shahnawz",
+            lastname: "Khan"
         }
     }
 }
-
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname);
 // console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj4 = {5: "a", 6: "b"}
+// if we want to confirm that any value is available in the object or not we use question mark ? for protection otherwise we have to use if else
+// console.log(regularUser.fullname?.userfullname.firstname);
 
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "a",
+    4: "b"
+}
+const obj4 = {
+    5: "a",
+    6: "b"
+}
+
+// if we add obj1 and obj2 in obj3 it will give obj in obj not a single object string values
 // const obj3 = { obj1, obj2 }
+
+// so we use assign() which gives us a single object
+// assign(target, source)
+// target will be {} while source will be obj1, obj2, obj4
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
+// spread operator
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
@@ -56,20 +75,30 @@ const users = [
 ]
 
 users[1].email
-// console.log(tinderUser);
+console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// keys helps in getting all keys [will return full object with key value]
+// [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.keys(tinderUser));
+
+// it returns only the values of objects
+// [ '123abc', 'Sammy', false ]
+console.log(Object.values(tinderUser));
+
+// every key value pair is framed in Array first value will be key and second will be value
+// [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 const course = {
-    coursename: "js in hindi",
+    coursename: "Code with JavaScript",
     price: "999",
-    courseInstructor: "hitesh"
+    courseInstructor: "Shahnawaz"
 }
+
+// to destructure any value
 
 // course.courseInstructor
 
