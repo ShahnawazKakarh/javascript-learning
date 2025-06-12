10fe54f77dd085762bfefd173224d091b653dae7
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-06-12 12:15:16 +0000

    Arrow functions

--- 04_functions/03_arrow.js
+++ 04_functions/03_arrow.js
@@ -1,34 +1,42 @@
+
+// this: refers the current context
+// this is used within the current context or scope
 const user = {
-    username: "hitesh",
+    username: "KhanShahnawaz",
     price: 999,
 
+    // method for welcome message which is function
+    // whoever comes inside this method gets welcome message
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
         console.log(this);
     }
-
 }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+// values changes according to .this (context)
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
 
-// console.log(this);
+// {} object will printed 
+console.log(this);
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+function chai(){
+    let username = "Shahnawaz"
+    console.log(this.username);
+}
 
-// chai()
+chai()
 
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+// this can be used in objects not inside the function/
+// it will print undefined
+const chai1 = function () {
+    let username = "Shahnawazkhan"
+    console.log(this.username);
+}
 
-const chai =  () => {
-    let username = "hitesh"
+const chai2 =  () => {
+    let username = "Khan"
     console.log(this);
 }
 
@@ -43,7 +51,7 @@ const chai =  () => {
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "shahkhan"})
 
 
 console.log(addTwo(3, 4))
