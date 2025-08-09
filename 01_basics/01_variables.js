44118694cf8b73f1051aa2b6efd35e9391ddb4af
Shahnawaz Khan <55447546+ShahnawazKakarh@users.noreply.github.com>
2025-08-09 11:57:24 +0000

    variables changes and added some comments and values

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,24 +1,30 @@
 const accountId = 123
+
 let accountEmail = "shahnawaz@google.com"
 var accountPassword = "shahnawaz"
 accountCity = "Lahore"
+
+// accountId = 2 // TypeError: Assignment to constant variable. accountId is already declared with CONST which can't be changed.
+
+
 let accountState;
 
 accountEmail = "hr@google.com"
 accountPassword = "123"
 accountCity = "Faisalabad"
 
-console.log(accountId)
+// console.log(accountId)
 
-console.table([accountId, accountEmail, accountPassword, accountState])
+// use square brackets and using table we can input all variable altogether besides calling seperately.
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
 /*
 Summary
 
 Variables and Data Types
-const = can't be re-assigned a value and can't be declared again  (block scope)
-let = can be re-assigned a value but can't be declared again (block scope)
-var = it can be re-assigned a value and it can also be declared again  (Global scope)
+const = can't be re-assigned a value and can't be declared again. (so, it is a block scope)
+let = can be re-assigned a value but can't be declared again. (so, it is a block scope)
+var = it can be re-assigned a value and it can also be declared again. (It has a Global scope)
 
 => Never use var in your code instead of let because of block and functional scope issues.
 # console.table([group of variables you want to show in tabular form])
